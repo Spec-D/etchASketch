899205548da60413c11b64de68fa0030212c50ba
Derek Praegitzer <dpraegitzer@gmail.com>
2022-08-30 16:47:50 -0700

    more logic & layout testing - switched from div grid to table

--- javascript.js
+++ javascript.js
@@ -1,15 +1,44 @@
-window.onload = defaultGrid();
+const defaultGridSize = 16;
+const defaultColor = "black";
 
 let gridSize = 16;
+let currentColor = "black";
 
 function defaultGrid() {
-
+	
 	let grid = document.getElementById("gridContainer");
+	
+	let width = ((1 / gridSize) * 100); 
+	
+	window.alert("width: " + width);
 
-	for(let i=1; i<16; i++) {
-		for(let j=1; j<16; j++) {
-			grid.innerHTML += "<div id='pixel'>"
+	document.getElementsByClassName("pixel").style.width(width);
+
+
+	for(let i=1; i<defaultGridSize; i++) {
+		if (i == 1) {grid.innerHTML = grid.innerHTML + "<tr>";}
+		
+		for(let j=1; j<defaultGridSize; j++) {
+			grid.innerHTML = grid.innerHTML + "<td class='pixel'>" + j + "</td>";
 		}
+		
+		if (i == defaultGridSize) {grid.innerHTML = grid.innerHTML + "</tr>";}
+
 	}
 }
+
+defaultGrid();
+
+function updateGrid(gridSize) {
+	
+	// code...
+
+}
+
+function reset() {
+
+	// code...
+
+}
+
 
